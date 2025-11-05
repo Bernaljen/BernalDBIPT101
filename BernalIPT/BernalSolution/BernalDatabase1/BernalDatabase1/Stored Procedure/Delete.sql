258f02e49c3c3df7cd6de6db4f4e04c90b02451d
Bernaljen <143572704+Bernaljen@users.noreply.github.com>
2025-11-05 20:52:46 +0800

    Delete BernalIPT/BernalSolution directory

--- BernalIPT/BernalSolution/BernalDatabase1/BernalDatabase1/Stored Procedure/Delete.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[Delete]
-     @Customer INT = Null
-AS
-BEGIN
-     DELETE FROM [dbo].[Table1] WHERE CustomerId = CustomerId
-END
-
-
--- BernalIPT/BernalSolution/BernalDatabase1/BernalDatabase1/Table/Table1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table1]
-(
-	[CustomerId] INT NOT NULL PRIMARY KEY, 
-    [CustomerName] NCHAR(10) NULL, 
-    [ContactNumber] NCHAR(10) NULL, 
-    [RoomNumber] NCHAR(10) NULL, 
-    [RoomType] NCHAR(10) NULL
-)
