1526fc040af98e471be9947bf2a24d084f5be7c8
Bernaljen <143572704+Bernaljen@users.noreply.github.com>
2025-11-05 20:56:54 +0800

    Add files via upload

---
+++ Bernal_IPT/BernalFramework/Commands/DeleteYouTubeViewerCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BernalIPT101.BernalDomain.Commands;
+using BernalIPT101.BernalFramework.DTOs;
+
+namespace BernalIPT101.BernalFramework.Commands
+{
+    public class DeleteYouTubeViewerCommand : IDeleteYouTubeViewerCommand
+    {
+        private readonly YouTubeViewersDbContextFactory _contextFactory;
+
+        public DeleteYouTubeViewerCommand(YouTubeViewersDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task Execute(Guid id)
+        {
+            using (YouTubeViewersDbContext context = _contextFactory.Create())
+            {
+                YouTubeViewerDto youTubeViewerDto = new YouTubeViewerDto()
+                {
+                    Id = id,
+                };
+
+                context.YouTubeViewers.Remove(youTubeViewerDto);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
